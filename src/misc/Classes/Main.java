26c2c24dfdfacac04de2892b4eb052f2f8dad7d4
bhuvitw <bhuvnesh6115@gmail.com>
2025-10-29 20:38:02 +0530

    complete LinkedList Code

---
+++ src/misc/Classes/Main.java
@@ -0,0 +1,15 @@
+package misc.Classes;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        Cookie cookieOne = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
+
+        cookieOne.setColor("Yellow");
+
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
+    }
+}
